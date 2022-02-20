f9e1e07627ddec3467509be116954f7b0cb549fb
OleksandrKudriavtsev <oleksandrkudriavtsev.dev@gmail.com>
2022-02-20 16:05:41 +0200

    Деструктуризация
    Деструктуризация переменных

--- app.js
+++ app.js
@@ -1,65 +1,92 @@
-export default [
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg",
-    description: "Hokkaido Flower",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
-    description: "Container Haulage Freight",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",
-    description: "Aerial Beach View",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",
-    description: "Flower Blooms",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",
-    description: "Alpine Mountains",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",
-    description: "Mountain Lake Sailing",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
-    description: "Alpine Spring Meadows",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
-    description: "Nature Landscape",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
-    description: "Lighthouse Coast Sea",
-  },
-];
+import galleryItems from "./js/images.js";
+
+////////////////////////////////////// ССЫЛКИ ///////////////////////////////////////////////////////////
+
+const refs = {
+  galleryList: document.querySelector(".js-gallery"),
+  overlayDiv: document.querySelector(".js-lightbox"),
+  lightboxOverlay: document.querySelector(".lightbox__overlay"),
+  image: document.querySelector(".lightbox__image"),
+  btnClose: document.querySelector('[data-action="close-lightbox"]'),
+};
+
+const { galleryList, overlayDiv, lightboxOverlay, image, btnClose } = refs;
+
+////////////////////////////////////// СОЗАНИЕ РАЗМЕТКИ ГАЛЛЕРЕИ ///////////////////////////////////////
+
+const itemEl = ({ preview, original, description }) => {
+  return `<li class="gallery__item">
+            <a href="" class="gallery__link"> 
+              <img class="gallery__image" src=${preview} alt=${description} data-source=${original} width="100%" heigth="100%">
+            </a>
+          </li>`;
+};
+
+const listEl = galleryItems.map(itemEl).join("");
+
+galleryList.insertAdjacentHTML("beforeend", listEl);
+
+////////////////////////////////////// ОТКРЫТИЕ МОДАЛКИ ////////////////////////////////////////////////
+
+galleryList.addEventListener("click", onGalleryOpen);
+
+function onGalleryOpen(event) {
+  event.preventDefault();
+  overlayDiv.classList.add("is-open");
+  image.src = event.target.dataset.source;
+  image.alt = event.target.dataset.alt;
+
+  window.addEventListener("keydown", onEscBtnPress);
+  window.addEventListener("keydown", arrowSlider);
+}
+
+////////////////////////////////////// ЗАКРЫТИЕ МОДАЛКИ ////////////////////////////////////////////////
+
+btnClose.addEventListener("click", onGalleryClose);
+
+function onGalleryClose() {
+  overlayDiv.classList.remove("is-open");
+  image.src = "";
+
+  window.removeEventListener("keydown", onEscBtnPress);
+  window.removeEventListener("keydown", arrowSlider);
+}
+
+////////////////////////////////////// ЗАКРЫТИЕ ЭСКЕЙПОМ ///////////////////////////////////////////////
+
+function onEscBtnPress(event) {
+  const ESC = "Escape";
+  if (event.code === ESC) {
+    onGalleryClose();
+  }
+}
+
+////////////////////////////////////// ЗАКРЫТИЕ МИСКЛИКОМ /////////////////////////////////////////////
+
+lightboxOverlay.addEventListener("click", onGalleryMissClick);
+
+function onGalleryMissClick() {
+  if (lightboxOverlay) {
+    onGalleryClose();
+  }
+}
+
+////////////////////////////////////// СКРОЛЛ СТРЕЛКАМИ //////////////////////////////////////////////
+
+function arrowSlider(event) {
+  let nextIndex = 0;
+
+  const currentIndex = galleryItems.indexOf(
+    galleryItems.find((item) => item.description === image.alt)
+  );
+
+  if (event.key === "ArrowRight") {
+    nextIndex = currentIndex < galleryItems.length - 1 ? currentIndex + 1 : 0;
+  }
+  if (event.key === "ArrowLeft") {
+    nextIndex = currentIndex > 0 ? currentIndex - 1 : galleryItems.length - 1;
+  }
+
+  image.src = galleryItems[nextIndex].original;
+  image.alt = galleryItems[nextIndex].description;
+}
